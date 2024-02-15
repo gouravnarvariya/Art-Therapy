9afe8aafd297027d48d1b493442c5f597bb5d96a
Gourav Narvariya <gouravnarvariya7@gmail.com>
2024-02-15 13:55:27 +0530

    check

---
+++ custom.js
@@ -0,0 +1,26 @@
+const toggleMenu = document.querySelector(".toggle-menu")
+const showBar = document.querySelector(".hiddenNavbar")
+toggleMenu.addEventListener('click',()=>{
+    console.log("hello")
+    showBar.classList.toggle("showNavbar")
+})
+
+  function submitForm() {
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const message = document.getElementById('message').value;
+
+    // Add data to Cloud Firestore
+    db.collection("contacts").add({
+        email: email,
+        phone: phone,
+        message: message
+    })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+        // Optionally, redirect or show a success message
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+}
