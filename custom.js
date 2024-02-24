192b571aa9e0378e1193816344220f7e8e8ecb0f
Gourav Narvariya <gouravnarvariya7@gmail.com>
2024-02-24 14:19:43 +0530

    changes

--- custom.js
+++ custom.js
@@ -1,49 +1,5 @@
-  
-// const showNavbar = document.querySelector(".nav-section")
 
-// document.addEventListener('scroll', function() {
-//     var scrollHeight = window.scrollY;
-//     var screenSize = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-//     var leftElement = document.querySelector('.left');
-//     var rightElement = document.querySelector('.right');
-// //     console.log(leftElement)
-// //    console.log(scrollHeight)
-   
-
-//     if ((scrollHeight >= 788 && screenSize > 601) || (scrollHeight >= 700 && screenSize < 600)) {
-//         showNavbar.classList.remove("animate__fadeOutUp");
-//         showNavbar.classList.add("nav-flex-1");
-//           showNavbar.classList.add("animate__fadeInDown", function() {
-//             // This function will be called once the showNavbar animation is complete
-//             handleLeftRightAnimation();
-//             console.log("first");
-//         });
-//     } else {
-//         showNavbar.classList.add("animate__fadeOutUp");
-//         showNavbar.classList.remove("animate__fadeInDown");
-//     }
-//     function handleLeftRightAnimation() {
-//         if ((scrollHeight >= 788 && screenSize > 601) || (scrollHeight >= 700 && screenSize < 600)) {
-//             // Add classes to leftElement and rightElement
-//             leftElement.classList.add('animate__fadeInLeft');
-//             rightElement.classList.add('animate__fadeInRight');
-//             console.log("if")
-//         } else {
-//             leftElement.classList.remove('animate__fadeInLeft');
-//             rightElement.classList.remove('animate__fadeInRight');
-//             console.log("else")
-//         }
-//     }
-//     // if ((scrollHeight >= 788 && screenSize > 601) || (scrollHeight >= 700 && screenSize < 600)) {
-//     //     leftElement.classList.add('animate__fadeInLeft');
-//     //     rightElement.classList.add('animate__fadeInRight');
-       
-//     // } else {
-//     //     leftElement.classList.remove('animate__fadeInLeft');
-//     //     rightElement.classList.remove('animate__fadeInRight');
-       
-//     // }
-// });
+// animation of navbar and brains animation of brains animation of navbar and brains 
 
 const showNavbar = document.querySelector(".nav-section");
 let animationApplied = false;
@@ -101,7 +57,7 @@ document.addEventListener('scroll', function() {
 });
 
 
-
+// Toggle   Toggle  Toggle  Toggle  Toggle  Toggle  Toggle  Toggle 
 
 const toggleMenu = document.querySelector(".toggle-menu")
 const showBar = document.querySelector(".hiddenNavbar")
@@ -135,7 +91,7 @@ gsap.timeline({
 
 
 
-
+// form submit form submit form submit form submit form submit 
 
 
 
@@ -158,77 +114,6 @@ function submitForm() {
         console.error("Error adding document: ", error);
     });
 }
-
-
-// type animation type animation type animation type animation type animation 
-
-// const typedTextSpan = document.querySelector(".typed-text");
-// const cursorSpan = document.querySelector(".cursor");
-
-// const textArray = ["Online-program", "Offline-program", "one-on-one", "Customised group"];
-// const typingDelay = 200;
-// const erasingDelay = 100;
-// const newTextDelay = 2000; // Delay between current and next text
-// let textArrayIndex = 0;
-// let charIndex = 0;
-
-// function type() {
-//   if (charIndex < textArray[textArrayIndex].length) {
-//     if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-//     typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-//     charIndex++;
-//     setTimeout(type, typingDelay);
-//   } 
-//   else {
-//     cursorSpan.classList.remove("typing");
-//   	setTimeout(erase, newTextDelay);
-//   }
-// }
-
-// function erase() {
-// 	if (charIndex > 0) {
-//     if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-//     typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
-//     charIndex--;
-//     setTimeout(erase, erasingDelay);
-//   } 
-//   else {
-//     cursorSpan.classList.remove("typing");
-//     textArrayIndex++;
-//     if(textArrayIndex>=textArray.length) textArrayIndex=0;
-//     setTimeout(type, typingDelay + 1100);
-//   }
-// }
-
-// document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-//   if(textArray.length) setTimeout(type, newTextDelay + 250);
-// });
-
-
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     const slider = document.querySelector('.slider');
-//     const slides = document.querySelectorAll('.slider .therapy-inner-div');
-
-//     let index = 0;
-
-//     function showNextSlide() {
-//         slides.forEach((slide) => {
-//             slide.style.display = 'none';
-//         });
-
-//         index = (index + 1) % slides.length;
-//         slides[index].style.display = 'block';
-//     }
-
-//     // Initial setup
-//     slides.forEach((slide, i) => {
-//         slide.style.display = i === 0 ? 'block' : 'none';
-//     });
-
-//     setInterval(showNextSlide, 2000); // Change slide every 2000 milliseconds (2 seconds)
-// });
-
 
 
 
@@ -266,6 +151,84 @@ sections.forEach((eachPanel, i) => {
     onEnterBack: () => goToSection(i)
   });
 });
+
+
+
+
+
+
+// type animation type animation type animation type animation type animation 
+
+
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Tap into your potential", "Unlock your emotional", "Master your gift", "Bring excellence to your work"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const slider = document.querySelector('.slider');
+    const slides = document.querySelectorAll('.slider .therapy-inner-div');
+
+    let index = 0;
+
+    function showNextSlide() {
+        slides.forEach((slide) => {
+            slide.style.display = 'none';
+        });
+
+        index = (index + 1) % slides.length;
+        slides[index].style.display = 'block';
+    }
+
+    // Initial setup
+    slides.forEach((slide, i) => {
+        slide.style.display = i === 0 ? 'block' : 'none';
+    });
+
+    setInterval(showNextSlide, 2000); // Change slide every 2000 milliseconds (2 seconds)
+});
+
+
 
 
 
